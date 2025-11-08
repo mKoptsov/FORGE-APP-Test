76b43a6f55f40710182fa6da1602aa2a5cd6d476
mkopt_sftsrv <mkopt@softserveinc.com>
2025-11-08 10:46:39 +0100

    NONE:added this one

--- src/webtriggers/process-github-event.ts
+++ src/webtriggers/process-github-event.ts
@@ -8,12 +8,10 @@ export const trigger = async (req: any) => {
 
   if (!code) return { error: "Missing code" };
 
-  const clientId = "Ov23li4cXVVJau5GC7tn";
-  const clientSecret = "a5983fb4d76a78361b96826d4a9890688271a11e";
 
   const client = new GithubClient("https://github.com");
 
-  const token = await client.getToken(clientId, clientSecret, code);
+  const token = await client.getToken('', '', code);
 
   if (!token.access_token){
     return { error: "Failed to get GitHub token", details: token };
